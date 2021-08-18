403160d94db492398a3266e492090ba9abf5e95b
Patryk.Piechaczek <patryk.piechaczek@bottomline.com>
2021-08-18 23:03:45 +0200

    Remove unused code

--- src/main/java/com/patres/ownframework/step2b/application/Step2bApp.java
+++ src/main/java/com/patres/ownframework/step2b/application/Step2bApp.java
@@ -5,37 +5,15 @@ import com.patres.ownframework.step2b.application.model.Company;
 import com.patres.ownframework.step2b.application.service.CompanyService;
 import com.patres.ownframework.step2b.framework.ProxyMethodInterceptor;
 import net.sf.cglib.proxy.Enhancer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Step2bApp {
 
-    private static final Logger logger = LoggerFactory.getLogger(Step2bApp.class);
-
     public static void main(String[] args) {
-        /* 1 */ final Enhancer enhancer = new Enhancer();
-        /* 2 */ enhancer.setSuperclass(CompanyService.class);
-        /* 3 */ enhancer.setCallback(new ProxyMethodInterceptor());
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(CompanyService.class);
+        enhancer.setCallback(new ProxyMethodInterceptor());
 
-        /* 4 */ CompanyService companyService = (CompanyService) enhancer.create(new Class[]{CompanyDao.class}, new Object[]{new CompanyDao()});
+        CompanyService companyService = (CompanyService) enhancer.create(new Class[]{CompanyDao.class}, new Object[]{new CompanyDao()});
         companyService.createCompany(new Company());
     }
 }
-
-// Since JDK 16, the default for the --illegal-access option is deny, so “deep reflection” to JDK classes fails.
-// https://github.com/cglib/cglib/issues/191
-// --illegal-access=permit
-
-/*
-        enhancer.setCallback((MethodInterceptor) (obj, method, methodArgs, proxy) -> {
-            try {
-                logger.info("TRANSACTION:   BEGIN");
-                final Object invoke = proxy.invokeSuper(obj, methodArgs);
-                logger.info("TRANSACTION:   COMMIT");
-                return invoke;
-            } catch (Exception e) {
-                logger.info("TRANSACTION:   ROLLBACK");
-                throw e;
-            }
-        });
- */
